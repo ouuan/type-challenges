ddfa0c4607c557d8a17b5c0bb18d4ae3c3e8c50b
Yufan You <ouuansteve@gmail.com>
2021-11-05 00:37:26 +0800

    finish: 43-easy-exclude

--- questions/43-easy-exclude/template.ts
+++ questions/43-easy-exclude/template.ts
@@ -1,1 +1,8 @@
-type MyExclude<T, U> = any
+// Long answer:
+// type Helper<T, U> = {
+//   [key in T as key extends U ? never : string]: key;
+// }
+// type MyExclude<T, U> = Helper<T, U>[keyof Helper<T, U>]
+
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+type MyExclude<T, U> = T extends U ? never : T
