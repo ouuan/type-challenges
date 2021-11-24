904e57cfc04fd3e38af7a87b9044a94e892a8993
Yufan You <ouuansteve@gmail.com>
2021-11-24 19:54:56 +0800

    finish: 108-medium-trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,2 @@
-type Trim<S extends string> = any
+type TrimSpace = ' ' | '\n' | '\t'
+type Trim<S extends string> = S extends `${Space}${infer R}` ? Trim<R> : (S extends `${infer L}${Space}` ? Trim<L> : S)
