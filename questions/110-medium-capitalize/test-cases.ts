3592f66b6565ca6cb9070cecd3333442ea05e181
Yufan You <ouuansteve@gmail.com>
2021-11-25 23:00:37 +0800

    finish: 110-medium-capitalize

--- questions/110-medium-capitalize/test-cases.ts
+++ questions/110-medium-capitalize/test-cases.ts
@@ -1,8 +1,11 @@
 import { Equal, Expect } from '@type-challenges/utils'
+import { Capitalize } from './template'
 
 type cases = [
   Expect<Equal<Capitalize<'foobar'>, 'Foobar'>>,
   Expect<Equal<Capitalize<'FOOBAR'>, 'FOOBAR'>>,
   Expect<Equal<Capitalize<'foo bar'>, 'Foo bar'>>,
+  Expect<Equal<Capitalize<'ouuan'>, 'Ouuan'>>,
+  Expect<Equal<Capitalize<'a'>, 'A'>>,
   Expect<Equal<Capitalize<''>, ''>>,
 ]
