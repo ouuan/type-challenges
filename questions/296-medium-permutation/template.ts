00bd00abcfd64ea49128aa2d955056efeef852a1
Yufan You <ouuansteve@gmail.com>
2021-12-08 09:30:55 +0800

    finish: 296-medium-permutation

--- questions/296-medium-permutation/template.ts
+++ questions/296-medium-permutation/template.ts
@@ -1,1 +1,4 @@
-type Permutation<T> = any
+type Permutation<T, K = T> = [T] extends [never] ? [] : T extends T ? [T, ...Permutation<Exclude<K, T>>] : []
+
+// `[T] extends [never]` checks that T is never, while `T extends never` does not
+// `T extends T` iterates in T, and then T becomes one of the original T so we need K as the original T
